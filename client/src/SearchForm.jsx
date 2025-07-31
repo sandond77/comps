02efd906795d391d115a6459bacbae02effb900f
sandond77 <sandond77@gmail.com>
2025-07-31 11:20:55 -0700

    connected searchForm to state

--- client/src/SearchForm.jsx
+++ client/src/SearchForm.jsx
@@ -4,37 +4,100 @@ import ClearIcon from '@mui/icons-material/Clear';
 import React, { useState } from 'react';
 
 export default function SearchForm({ handleSubmit }) {
-	const [grade, setGrade] = useState('');
-	const [cardName, setCardName] = useState('');
-	const [cardNumber, setCardNumber] = useState('');
-	const [cardLanguage, setCardLanuguage] = useState('');
-	const [setName, setSetName] = useState('');
+	const [formData, setFormData] = useState({
+		grade: '',
+		cardName: '',
+		cardGame: '',
+		cardNumber: '',
+		cardLanguage: '',
+		setName: ''
+	});
 
 	const handleReset = (event) => {
 		event.preventDefault();
 		console.log('reset hit');
-		event.target.value = '';
+		setFormData({
+			grade: '',
+			cardName: '',
+			cardGame: '',
+			cardNumber: '',
+			cardLanguage: '',
+			setName: ''
+		});
 	};
+
+	const handleChange = (event) => {
+		setFormData((prevData) => ({
+			...prevData,
+			[event.target.name]: event.target.value
+		}));
+	};
+
 	return (
 		<Box component="form" noValidate autoComplete="off">
 			<Grid container spacing={2}>
 				<Grid size={{ xs: 12, md: 4 }}>
-					<TextField id="grade" label="Grade" fullWidth />
+					<TextField
+						id="grade"
+						label="grade"
+						name="grade"
+						fullWidth
+						value={formData.grade}
+						onChange={handleChange}
+					/>
 				</Grid>
 				<Grid size={{ xs: 12, md: 4 }}>
-					<TextField required id="cardName" label="Card Name" fullWidth />
+					<TextField
+						required
+						id="cardName"
+						label="Card Name"
+						name="cardName"
+						fullWidth
+						value={formData.cardName}
+						onChange={handleChange}
+					/>
 				</Grid>
 				<Grid size={{ xs: 12, md: 4 }}>
-					<TextField id="cardNumber" label="Card Number" fullWidth />
+					<TextField
+						id="cardNumber"
+						label="Card Number"
+						name="cardNumber"
+						fullWidth
+						value={formData.cardNumber}
+						onChange={handleChange}
+					/>
 				</Grid>
 				<Grid size={{ xs: 12, md: 6 }}>
-					<TextField required id="cardGame" label="Card Game" fullWidth />
+					<TextField
+						required
+						id="cardGame"
+						name="cardGame"
+						label="Card Game"
+						fullWidth
+						value={formData.cardGame}
+						onChange={handleChange}
+					/>
 				</Grid>
 				<Grid size={{ xs: 12, md: 6 }}>
-					<TextField required id="cardLanguage" label="Language" fullWidth />
+					<TextField
+						required
+						id="cardLanguage"
+						name="cardLanguage"
+						label="Language"
+						fullWidth
+						value={formData.cardLanguage}
+						onChange={handleChange}
+					/>
 				</Grid>
 				<Grid size={{ xs: 12 }}>
-					<TextField id="set" label="Set Name" fullWidth />
+					<TextField
+						id="set"
+						name="setName"
+						label="Set Name"
+						fullWidth
+						value={formData.setName}
+						onChange={handleChange}
+					/>
 				</Grid>
 			</Grid>
 			<Button
