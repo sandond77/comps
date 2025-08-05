49ebd4289382b0dd15c774cdb5828397f5e94d52
sandond77 <sandond77@gmail.com>
2025-08-04 22:10:55 -0700

    reworked ebay get route to filter out auction vs BIN listings

--- server/routes/api.js
+++ server/routes/api.js
@@ -12,12 +12,12 @@ async function getEbayAccessToken() {
 	).toString('base64');
 
 	const tokenRes = await axios.post(
-		'https://api.ebay.com/identity/v1/oauth2/token', // ✅ Production URL
+		'https://api.ebay.com/identity/v1/oauth2/token',
 		'grant_type=client_credentials&scope=https://api.ebay.com/oauth/api_scope',
 		{
 			headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
-				Authorization: `Basic ${credentials}` // base64(client_id:client_secret)
+				Authorization: `Basic ${credentials}`
 			}
 		}
 	);
@@ -30,21 +30,24 @@ async function getEbayAccessToken() {
 // /api/search?q=charizard
 router.get('/search', async (req, res) => {
 	const query = req.query.q;
+	const filter = req.query.filter;
 
 	try {
 		const token = await getEbayAccessToken();
 
-		const ebayRes = await axios.get(
-			`https://api.ebay.com/buy/browse/v1/item_summary/search?q=${encodeURIComponent(
-				query
-			)}`,
-			{
-				headers: {
-					Authorization: `Bearer ${token}`,
-					'X-EBAY-C-MARKETPLACE-ID': 'EBAY_US'
-				}
+		let ebayUrl = `https://api.ebay.com/buy/browse/v1/item_summary/search?q=${encodeURIComponent(
+			query
+		)}`;
+
+		if (filter === 'AUCTION' || filter === 'FIXED_PRICE') {
+			ebayUrl += `&filter=buyingOptions:{${filter}}`;
+		}
+		const ebayRes = await axios.get(ebayUrl, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'X-EBAY-C-MARKETPLACE-ID': 'EBAY_US'
 			}
-		);
+		});
 
 		// console.log('Authorization header:', `Bearer ${token}`);
 		// console.log('Browse API response:', ebayRes.data);
